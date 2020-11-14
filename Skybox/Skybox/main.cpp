befc4490a98103e2a745f0eaf8e6758478067540
Jess <SulaFelinae@gmail.com>
2020-11-13 18:03:05 -0800

    final start

---
+++ Skybox/Skybox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Skybox
+//
+//  Created by Liang Zhao on 11/13/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
