a276522f5781685b8673858edc8b0abb21992010
Jess <SulaFelinae@gmail.com>
2020-11-20 20:31:34 -0800

    skybox

--- Skybox/Skybox/skybox.fs
+++ Skybox/Skybox/skybox.fs
@@ -1,9 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
+in vec3 TexCoords;
+
+uniform samplerCube skybox;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = texture(skybox, TexCoords);
 }
