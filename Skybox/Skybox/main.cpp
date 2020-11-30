6bf7d0f00d8c3240be9c03e6a0dcf9d515de6c11
Jess <SulaFelinae@gmail.com>
2020-11-30 14:04:08 -0800

    change the ...

--- Skybox/Skybox/main.cpp
+++ Skybox/Skybox/main.cpp
@@ -19,8 +19,6 @@
 #include <MyGLFWLib/camera.h>
 #include <MyGLFWLib/model.h>
 
-
-
 #include <iostream>
 
 
@@ -425,3 +423,13 @@ unsigned int loadCubemap(vector<std::string> faces)
 
     return textureID;
 }
+
+
+#ifdef __APPLE__
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
