bdab6a2ef63494a8a132082c7ab755ac32a7b3c8
Jess <SulaFelinae@gmail.com>
2020-12-01 22:03:42 -0800

    tell GLFW to show mouse

--- Skybox/Skybox/main.cpp
+++ Skybox/Skybox/main.cpp
@@ -78,7 +78,7 @@ int main()
     glfwSetScrollCallback(window, scroll_callback);
 
     // tell GLFW to capture our mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
