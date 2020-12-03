a2387dda5fe1f4fbcf74e7c399f6e8f1e89c6215
Jess <SulaFelinae@gmail.com>
2020-12-03 11:05:17 -0800

    Done

--- Skybox/Skybox/main.cpp
+++ Skybox/Skybox/main.cpp
@@ -73,6 +73,7 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
@@ -185,6 +186,35 @@ int main()
     suitShader.use();
     suitShader.setInt("texture1", 3);
     
+#ifdef __APPLE__
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+
+#endif
+    
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    
+    // Our state
+    
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
     
     // render loop
     // -----------
@@ -243,9 +273,53 @@ int main()
         glBindVertexArray(0);
         glDepthFunc(GL_LESS); // set depth function back to default
         
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
         
-        
+        // Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            //static float f = 0.0f;
+            
+             glm::vec4 testmodel;
+             glm::vec4 testview;
+             glm::vec4 testprojection;
+             glm::vec3 testcameraPos;
+                
+            testmodel.x = 1.567;
+            testmodel.y = 4.678;
+            testmodel.w = 8.134;
 
+            testview.y = 0.643;
+            testview.z = 0.986;
+            testprojection.x = 0.453;
+            testprojection.z = 9.321;
+            testcameraPos.y = 3.145;
+            
+            ImGui::Begin("Debug >_< by Dear ImGui ");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is my final project");               // Display some text (you can use a format strings too)
+
+            ImGui::SliderFloat4("testmodel", &testmodel.x, 0.0f, 20.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat4("testview", &testview.x, 0.0f, 20.0f);
+            ImGui::SliderFloat4("testprojection", &testprojection.x, 0.0f, 20.0f);
+            ImGui::SliderFloat3("testcameraPos", &testcameraPos.x, 0.0f, 20.0f);
+            
+            ImGui::ColorEdit3("color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+        
+        
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -255,6 +329,12 @@ int main()
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
   
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    
+    
     glDeleteVertexArrays(1, &skyboxVAO);
     glDeleteBuffers(1, &skyboxVAO);
 
